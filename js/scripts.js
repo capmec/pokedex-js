79828f9b7668cb36edb5c9364a755e3d858b8067
capmec <capmec@gmail.com>
2024-03-06 13:19:25 +0100

    Exercise-1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,17 @@
-alert('Hello world');
-
-var favouriteFood = 'Ramen';
-
-document.write(favouriteFood);
+let pokemonList = [
+	{
+		name: 'Bulbasaur',
+		height: 0.7,
+		types: ['grass', 'poison'],
+	},
+	{
+		name: 'Vulpix',
+		height: 0.6,
+		types: ['fire'],
+	},
+	{
+		name: 'Vapereon',
+		height: 1,
+		types: ['water'],
+	},
+];
