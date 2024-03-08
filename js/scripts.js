4184b1dd8e63fed2faa76fde92a71c7477ac2ed7
capmec <capmec@gmail.com>
2024-03-08 18:11:10 +0100

    Exercise 1.3 for loop if - else

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,27 @@ let pokemonList = [
 	},
 	{
 		name: 'Vapereon',
-		height: 1,
+		height: 1.0,
 		types: ['water'],
 	},
 ];
+
+//create a for loop that iterates over each item in pokemonList array
+//write the name and height of each pokemon to the document
+//if the pokemon is larger or equal to 1.0 = Wow, that’s big!
+
+for (let i = 0; i < pokemonList.length; i++) {
+	if (pokemonList[i].height >= 1.0) {
+		document.write(
+			pokemonList[i].name +
+				' (height: ' +
+				pokemonList[i].height +
+				') - Wow, that’s big!',
+		);
+	} else {
+		document.write(
+			pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')',
+		);
+		document.write('<br>');
+	}
+}
