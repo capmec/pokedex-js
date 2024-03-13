75e5d8a8392e165b20221612cf915bd31147d743
capmec <capmec@gmail.com>
2024-03-13 15:30:20 +0100

    Exercise 1.5 - part 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 let pokemonList = [
 	{
+		name: 'Machamp',
+		height: 1.6,
+		types: ['Fighting'],
+	},
+	{
 		name: 'Bulbasaur',
 		height: 0.7,
 		types: ['grass', 'poison'],
@@ -11,27 +16,38 @@ let pokemonList = [
 	},
 	{
 		name: 'Vapereon',
-		height: 1.0,
+		height: 1.6,
 		types: ['water'],
 	},
 ];
 
-//create a for loop that iterates over each item in pokemonList array
-//write the name and height of each pokemon to the document
-//if the pokemon is larger or equal to 1.0 = Wow, that’s big!
-
-for (let i = 0; i < pokemonList.length; i++) {
-	if (pokemonList[i].height >= 1.0) {
+pokemonList.forEach(function (pokemon) {
+	if (pokemon.height > 1.0) {
 		document.write(
-			pokemonList[i].name +
+			pokemon.name +
+				' ' +
 				' (height: ' +
-				pokemonList[i].height +
-				') - Wow, that’s big!',
+				pokemon.height +
+				') - Wow, that’s big!' +
+				'<br>',
 		);
 	} else {
-		document.write(
-			pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')',
-		);
-		document.write('<br>');
+		document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>');
 	}
-}
+});
+
+// for (let i = 0; i < pokemonList.length; i++) {
+// 	if (pokemonList[i].height >= 1.0) {
+// 		document.write(
+// 			pokemonList[i].name +
+// 				' (height: ' +
+// 				pokemonList[i].height +
+// 				') - Wow, that’s big!',
+// 		);
+// 	} else {
+// 		document.write(
+// 			pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')',
+// 		);
+// 		document.write('<br>');
+// 	}
+// }
