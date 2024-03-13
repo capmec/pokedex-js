ac8183c73b6faddcf7efe43e1eb94856fd8afb3d
capmec <capmec@gmail.com>
2024-03-13 16:06:59 +0100

    exercise-1.5 testing for each

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-let pokemonRepository = function () {
+let pokemonRepository = (function () {
 	let pokemonList = [
 		{
 			name: 'Bulbasaur',
@@ -10,17 +10,11 @@ let pokemonRepository = function () {
 			height: 1.0,
 			types: ['water'],
 		},
-
 		{
 			name: 'Vulpix',
 			height: 0.6,
 			types: ['fire'],
 		},
-	];
-
-	//Function Parameters & Arguments
-
-	let pokemonList2 = [
 		{
 			name: 'Metapod',
 			height: 0.7,
@@ -36,56 +30,38 @@ let pokemonRepository = function () {
 			height: 1.6,
 			types: ['fighting'],
 		},
-		{
-			name: 'Bulbasaur',
-			height: 0.7,
-			types: ['grass', 'poison'],
-		},
-		{
-			name: 'Vulpix',
-			height: 0.6,
-			types: ['fire'],
-		},
-		{
-			name: 'Vapereon',
-			height: 1.6,
-			types: ['water'],
-		},
 	];
-};
 
-//create a for loop that iterates over each item in pokemonList array
-//write the name and height of each pokemon to the document
-//if the pokemon is larger or equal to 1.0 = Wow, that’s big!
+	return {
+		add: function (pokemon) {
+			pokemonList.push(pokemon);
+		},
+		getAll: function () {
+			return pokemonList;
+		},
+		displayPokemon: function () {
+			pokemonList.forEach(function (pokemon) {
+				if (pokemon.height > 1.0) {
+					document.write(
+						pokemon.name +
+							' ' +
+							' (height: ' +
+							pokemon.height +
+							') - Wow, that’s big!' +
+							'<br>',
+					);
+				} else {
+					document.write(
+						pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>',
+					);
+				}
+			});
+		},
+	};
+})();
 
-function printArraysDetails(list) {
-	for (let i = 0; i < list.length; i++) {
-		if (list[i].height >= 1.0) {
-			document.write(
-				list[i].name + ' (height: ' + list[i].height + ') - Wow, that’s big!',
-			);
-			document.write('<br>');
-		} else {
-			document.write(list[i].name + ' (height: ' + list[i].height + ')');
-			document.write('<br>');
-		}
-	}
-}
-
-pokemonList.forEach(function (pokemon) {
-	if (pokemon.height > 1.0) {
-		document.write(
-			pokemon.name +
-				' ' +
-				' (height: ' +
-				pokemon.height +
-				') - Wow, that’s big!' +
-				'<br>',
-		);
-	} else {
-		document.write(pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>');
-	}
-});
+pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
+pokemonRepository.displayPokemon();
 
 // for (let i = 0; i < pokemonList.length; i++) {
 // 	if (pokemonList[i].height >= 1.0) {
@@ -102,6 +78,19 @@ pokemonList.forEach(function (pokemon) {
 // 		document.write('<br>');
 // 	}
 // }
+// printArraysDetails(pokemonList);
+// printArraysDetails(pokemonList2);
 
-printArraysDetails(pokemonList);
-printArraysDetails(pokemonList2);
+// function printArraysDetails(list) {
+// 	for (let i = 0; i < list.length; i++) {
+// 		if (list[i].height >= 1.0) {
+// 			document.write(
+// 				list[i].name + ' (height: ' + list[i].height + ') - Wow, that’s big!',
+// 			);
+// 			document.write('<br>');
+// 		} else {
+// 			document.write(list[i].name + ' (height: ' + list[i].height + ')');
+// 			document.write('<br>');
+// 		}
+// 	}
+// }
