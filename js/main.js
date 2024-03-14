385e7ef4d6a020f21f8afd1b0fccab0c8a0b7d87
capmec <capmec@gmail.com>
2024-03-14 19:41:07 +0100

    Exercise-1.6

--- js/main.js
+++ js/main.js
@@ -1,222 +1,60 @@
-// //forEach() Loops
+//TRAVERSING THE DOM
 
-// // let names = ['John', 'Anne', 'Carly'];
+/**Selecting Nodes */
 
-// console.log(
-// 	'You could loop through the array using the traditional for block:',
-// );
-// let nameList = ['John', 'Anne', 'Carly'];
+// let inputs = document.querySelectorAll('input');
+// console.log(inputs.length); // e.g., 2
 
-// for (let i = 0; i < nameList.length; i++) {
-// 	console.log(nameList[i]);
-// }
-
-// console.log(
-// 	'Another way you could iterate over this array is by using the forEach() function as follows:',
-// );
-
-// let nameListFE = ['John', 'Anne', 'Carly'];
-
-// nameListFE.forEach(function (name) {
-// 	console.log(name);
+// inputs.forEach(function (input) {
+// 	// do something with each input
 // });
 
-// let userList = [
-// 	{
-// 		name: 'Liz',
-// 		age: 20,
-// 	},
-// 	{
-// 		name: 'John',
-// 		age: 30,
-// 	},
-// 	{
-// 		name: 'Sammy',
-// 		age: 40,
-// 	},
-// ];
+// inputs[0]; // Get the first input
 
-// console.log(
-// 	'Logging the details of this list using a traditional for loop would look something like this:',
-// );
+// /* COMPLETE THE CODE BELOW BY FOLLOWING THE INSTRUCTIONS */
 
-// for (let i = 0; i < userList.length; i++) {
-// 	console.log(userList[i].name + ' is ' + userList[i].age + ' years old.');
-// }
+// // 1. Select and assign only the payments div:
+// let payments = document.querySelector('.payments');
 
-// let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+// // 2. Select and assign all divs with the class 'amount' INSIDE (!) the payments div
+// let amountsOfPayments = document.querySelectorAll('.amountOfPayments');
 
-// //external function
-// console.log('External Function');
-// myArray.forEach(logToConsole);
-
-// function logToConsole(item) {
-// 	console.log(item);
-// }
-
-// //internal anonymous function
-// console.log('internal anonymous function');
-// myArray.forEach(function (item) {
-// 	console.log(item);
+// // 3. For each amount inside payments, log the element in the console.
+// amountsOfPayments.forEach(function (amount) {
+// 	console.log(amount);
 // });
 
-// //arrow function
-// // {} is not necessary when there is only one line
-// console.log('arrow function');
-// myArray.forEach((item) => console.log(item));
+let mainTitle = document.querySelector('h1');
+console.log(mainTitle.innerText);
+mainTitle.innerText = 'THIS IS EPIC';
+console.log(mainTitle.innerText);
 
-// (function () {
-// 	let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// 	let str = '';
+// let container = document.querySelector('.container');
+// container.innerHTML = '<button>Click me!</button>';
+// console.log(container.innerHTML);
 
-// 	myArray.forEach(makeRainbowDivs);
+let container = document.querySelector('.container');
+let button = document.createElement('button');
+button.innerText = 'Click me!';
+container.appendChild(button);
 
-// 	function makeRainbowDivs(item) {
-// 		str += `<div> I am div ${item}</div>`;
-// 	}
+let elementToRemove = document.querySelector('p');
+elementToRemove.parentElement.removeChild(elementToRemove);
 
-// 	document.getElementById('container').innerHTML = str;
-// })();
+// element = document.querySelector('div');
+// console.log(element.tagName);
 
-// (function () {
-// 	let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// 	let str = '';
+let element = document.querySelector('.container');
 
-// 	myArray.forEach(function (item) {
-// 		str += `<div> I am div ${item}</div>`;
-// 	});
+console.log(element.getAttribute('id'));
+console.log(element.hasAttribute('value'));
+element.setAttribute('id', 'new-id');
 
-// 	document.getElementById('container').innerHTML = str;
-// })();
+console.log(element.getAttribute('id'));
 
-//FOR EACH USING ARROW FUNCTION
-// (function () {
-// 	let myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// 	let str = '';
+element.classList.contains('container');
+element.classList.add('new-class');
+element.classList.remove('new-class');
+element.classList.toggle('new-class');
 
-// 	myArray.forEach((item) => (str += `<div> I am div ${item}</div>`));
-
-// 	document.getElementById('container').innerHTML = str;
-// })();
-
-//OBJECT KEYS
-
-// let anne = {
-// 	name: 'Anne',
-// 	age: 38,
-// 	children: [],
-// };
-
-// Object.keys(anne).forEach(function (property) {
-// 	console.log(anne[property]);
-// 	document.write(property + ': ' + anne[property] + '<br>');
-// });
-
-//FUNCTIONAL PROGRAMMING
-
-// foodList = ['Pizza', 'Salad', 'Tuna'];
-
-// let anne = {
-// 	name: 'Anne Smith',
-// 	age: 38,
-// 	hasChildren: false,
-// };
-
-// function getPersonDescription(person) {
-// 	let ageDescription = person.age + ' years old';
-// 	let ChildrenDescription = person.hasChildren
-// 		? 'has children'
-// 		: 'has no children';
-
-// 	return person.name + ', ' + ageDescription + ', ' + ChildrenDescription;
-// }
-// console.log(getPersonDescription(anne));
-
-// var pug = {
-// 	age: 4,
-// 	hasPups: false,
-// 	name: 'Fred',
-// };
-
-// function getAgeDescription() {
-// 	return pug.age;
-// }
-
-// function getPupDescription(hasPups) {
-// 	return hasPups ? 'has pups' : 'has no pups';
-// }
-// function getDogDescription(dog) {
-// 	var ageDescription = getAgeDescription(dog.age);
-// 	var pupDescription = getPupDescription(dog.hasPups);
-
-// 	return dog.name + ', ' + ageDescription + ', ' + pupDescription;
-// }
-
-// console.log(getDogDescription(pug));
-
-// let add = (a, b) => a + b;
-// let multiply = (a, b) => a * b;
-
-// console.log(add(2, multiply(4, 2)));
-
-// function getFullName(person) {
-// 	return person.firstName + ' ' + person.lastName;
-// }
-
-// let Character = {
-// 	firstName: 'Homer',
-// 	lastName: 'Simpson',
-// };
-
-// let fullName = getFullName(Character);
-
-// console.log(fullName);
-
-// let app = function () {
-// 	let data = 'John';
-// 	let somePrivateData = 'secrets';
-
-// 	return {
-// 		username: data,
-// 	};
-// };
-
-// Global Variables and IIFE
-
-// let pokemonRepository = (function () {
-// 	let pokemonList = []; //empty array
-
-// 	return {
-// 		add: function (pokemon) {
-// 			pokemonList.push(pokemon);
-// 		},
-// 		getAll: function () {
-// 			return pokemonList;
-// 		},
-// 	};
-// })();
-
-// console.log(pokemonRepository.getAll()); // [] empty array
-// pokemonRepository.add({ name: 'Pikachu' });
-// console.log(pokemonRepository.getAll());
-
-//OR
-
-let pokemonRepository = (function () {
-	let pokemonList = [];
-
-	function add(pokemon) {
-		pokemonList.push(pokemon);
-	}
-
-	function getAll() {
-		return pokemonList;
-	}
-
-	return {
-		add: add,
-		getAll: getAll,
-	};
-})();
-console.log(pokemonRepository.add({ name: 'Pikachu' }));
-console.log(pokemonRepository.getAll());
+console.log(element.classList);
--- js/scripts.js
+++ js/scripts.js
@@ -32,65 +32,67 @@ let pokemonRepository = (function () {
 		},
 	];
 
-	return {
-		add: function (pokemon) {
+	function add(pokemon) {
+		if (
+			typeof pokemon === 'object' &&
+			'name' in pokemon &&
+			'height' in pokemon &&
+			'types' in pokemon
+		) {
 			pokemonList.push(pokemon);
-		},
-		getAll: function () {
-			return pokemonList;
-		},
-		displayPokemon: function () {
-			pokemonList.forEach(function (pokemon) {
-				if (pokemon.height > 1.0) {
-					document.write(
-						pokemon.name +
-							' ' +
-							' (height: ' +
-							pokemon.height +
-							') - Wow, that’s big!' +
-							'<br>',
-					);
-				} else {
-					document.write(
-						pokemon.name + ' (height: ' + pokemon.height + ')' + '<br>',
-					);
-				}
-			});
-		},
+		} else {
+			console.log('Pokemon attribute is missing');
+		}
+	}
+
+	function getAll() {
+		return pokemonList;
+	}
+
+	function addListItem(pokemon) {
+		// create a variable inside the forEach's loop function block, then assign it the ul element you just added to your “index.html” file. (Hint: use document.querySelector). All the rest of the steps will take place inside your forEach loop block.
+
+		let ul = document.querySelector('.pokemon-list');
+
+		//Create an li element (e.g., let listItem = document.createElement('li')).
+
+		let listItem = document.createElement('li');
+		// listItem.innerHTML = pokemon.name;
+
+		// Create a button element (e.g., let button = document.createElement('button')) and set its innerText to be the Pokémon's name (remember that forEach returns a Pokémon in each iteration).
+
+		let button = document.createElement('button');
+		button.innerText = pokemon.name;
+		//Add a class to the button using the classList.add method (button.classList.add(...)).
+
+		button.classList.add('button');
+
+		//Now, append the button to the list item as its child.
+
+		listItem.appendChild(button);
+
+		//Finally, append the list item to the unordered list as its child.
+		ul.appendChild(listItem);
+	}
+
+	return {
+		add: add,
+		getAll: getAll,
+		addListItem: addListItem,
 	};
 })();
 
+console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Pikachu', height: 0.4, types: ['electric'] });
-pokemonRepository.displayPokemon();
+console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+	pokemonRepository.addListItem(pokemon);
+});
 
-// for (let i = 0; i < pokemonList.length; i++) {
-// 	if (pokemonList[i].height >= 1.0) {
-// 		document.write(
-// 			pokemonList[i].name +
-// 				' (height: ' +
-// 				pokemonList[i].height +
-// 				') - Wow, that’s big!',
-// 		);
-// 	} else {
-// 		document.write(
-// 			pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')',
-// 		);
-// 		document.write('<br>');
-// 	}
-// }
-// printArraysDetails(pokemonList);
-// printArraysDetails(pokemonList2);
-
-// function printArraysDetails(list) {
-// 	for (let i = 0; i < list.length; i++) {
-// 		if (list[i].height >= 1.0) {
-// 			document.write(
-// 				list[i].name + ' (height: ' + list[i].height + ') - Wow, that’s big!',
-// 			);
-// 			document.write('<br>');
-// 		} else {
-// 			document.write(list[i].name + ' (height: ' + list[i].height + ')');
-// 			document.write('<br>');
-// 		}
-// 	}
-// }
+window.addEventListener('keydown', function (event) {
+	let survey_form = document.querySelector('#survey_form');
+	let isFormHidden = survey_form.classList.contains('hidden');
+	if (!isFormHidden && event.key === 'Escape') {
+		survey_form.classList.add('hidden');
+	}
+});
