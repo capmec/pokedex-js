d444d7ba6550eba9182e14c295e5a42406b3f572
capmec <capmec@gmail.com>
2024-03-19 19:23:54 +0100

    Exercise-1.7

--- js/main.js
+++ js/main.js
@@ -1,60 +1,74 @@
-//TRAVERSING THE DOM
+// //Synchronous vs. Asynchronous Code
 
-/**Selecting Nodes */
+// function runThisLater() {
+// 	console.log('Burgers');
+// }
 
-// let inputs = document.querySelectorAll('input');
-// console.log(inputs.length); // e.g., 2
+// console.log('Pizza');
+// setTimeout(runThisLater, 1000);
+// console.log('Salad');
 
-// inputs.forEach(function (input) {
-// 	// do something with each input
+// let count = 1;
+// function increaseCount() {
+// 	count = count + 1;
+// }
+// setTimeout(function () {
+// 	console.log('first call', count);
+// 	increaseCount();
+// }, 200);
+// setTimeout(function () {
+// 	console.log('second call', count);
+// 	increaseCount();
+// }, 100);
+// setTimeout(function () {
+// 	console.log('third call', count);
+// 	increaseCount();
+// }, 500);
+
+// //PROMISES
+
+// let examplePromise = new Promise(function (resolve, reject) {
+// 	let sum;
+// 	setTimeout(function () {
+// 		sum = 5 + 6;
+// 		if (sum > 10) {
+// 			resolve(sum);
+// 		} else {
+// 			reject('The promise has been rejected');
+// 		}
+// 	}, 2000);
 // });
+// console.log('some piece of code');
+// examplePromise
+// 	.then(function (result) {
+// 		console.log(result);
+// 	})
+// 	.catch(function (error) {
+// 		console.error(error);
+// 	});
+// console.log('another piece of code');
 
-// inputs[0]; // Get the first input
+function examplePromise() {
+	let promiseToReturn = new Promise(function (resolve, reject) {
+		let sum;
+		setTimeout(function () {
+			sum = 5 + 6;
+			if (sum > 10) {
+				resolve(sum);
+			} else {
+				reject('The promise has been rejected');
+			}
+		}, 2000);
+	});
+	return promiseToReturn;
+}
 
-// /* COMPLETE THE CODE BELOW BY FOLLOWING THE INSTRUCTIONS */
-
-// // 1. Select and assign only the payments div:
-// let payments = document.querySelector('.payments');
-
-// // 2. Select and assign all divs with the class 'amount' INSIDE (!) the payments div
-// let amountsOfPayments = document.querySelectorAll('.amountOfPayments');
-
-// // 3. For each amount inside payments, log the element in the console.
-// amountsOfPayments.forEach(function (amount) {
-// 	console.log(amount);
-// });
-
-let mainTitle = document.querySelector('h1');
-console.log(mainTitle.innerText);
-mainTitle.innerText = 'THIS IS EPIC';
-console.log(mainTitle.innerText);
-
-// let container = document.querySelector('.container');
-// container.innerHTML = '<button>Click me!</button>';
-// console.log(container.innerHTML);
-
-let container = document.querySelector('.container');
-let button = document.createElement('button');
-button.innerText = 'Click me!';
-container.appendChild(button);
-
-let elementToRemove = document.querySelector('p');
-elementToRemove.parentElement.removeChild(elementToRemove);
-
-// element = document.querySelector('div');
-// console.log(element.tagName);
-
-let element = document.querySelector('.container');
-
-console.log(element.getAttribute('id'));
-console.log(element.hasAttribute('value'));
-element.setAttribute('id', 'new-id');
-
-console.log(element.getAttribute('id'));
-
-element.classList.contains('container');
-element.classList.add('new-class');
-element.classList.remove('new-class');
-element.classList.toggle('new-class');
-
-console.log(element.classList);
+console.log('some piece of code');
+examplePromise()
+	.then(function (result) {
+		console.log(result);
+	})
+	.catch(function (error) {
+		console.log(error);
+	});
+console.log('another piece of code');
